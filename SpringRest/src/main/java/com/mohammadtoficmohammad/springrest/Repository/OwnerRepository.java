dc79545215e2db95820ba9953111a77ff823e3b5
MohammadToficMohammad <mohammad.tofic.mohammad@gmail.com>
2021-06-21 20:32:24 +0200

    clean up

--- SpringRest/src/main/java/com/mohammadtoficmohammad/springrest/Repository/OwnerRepository.java
+++ SpringRest/src/main/java/com/mohammadtoficmohammad/springrest/Repository/OwnerRepository.java
@@ -5,19 +5,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-
 import com.mohammadtoficmohammad.springrest.Models.Entity.Owner;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import com.mohammadtoficmohammad.springrest.Models.Entity.Owner;
-
-
 
 
 @Repository
-public interface OwnerRepository extends JpaRepository<Owner, Long>{
+public interface OwnerRepository extends JpaRepository<Owner, Long> {
 
 	@Transactional(propagation = Propagation.SUPPORTS)
 	@Query("SELECT o FROM Owner o JOIN FETCH o.cars WHERE o.id = (:id)")
-    public Owner findByIdAndFetchCarsEagerly(@Param("id") Long id);
+	public Owner findByIdAndFetchCarsEagerly(@Param("id") Long id);
 }
